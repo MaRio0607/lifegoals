d898d61d035836ed1b3b12a4ebf678bd6db1467d
Mario <57414832+MaRio0607@users.noreply.github.com>
2026-03-19 16:16:10 -0600

    Delete and write data localy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,10 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NgbModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    provideFirebaseApp(() => initializeApp({ "projectId": "lifegoals-807b6", "appId": "1:629776139532:web:7e7e1fb6dacdadac3c7529", "storageBucket": "lifegoals-807b6.firebasestorage.app", "apiKey": "AIzaSyBe05QBnOwkDKbYp5x3lIGJlTBOnuMpw3w", "authDomain": "lifegoals-807b6.firebaseapp.com", "messagingSenderId": "629776139532", })),
+    provideFirestore(() => getFirestore())
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
